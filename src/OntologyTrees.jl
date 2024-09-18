68d3ed9c4be30a12a9d127f54b7a1d161215f5a5
Christopher Damour <129872252+damourChris@users.noreply.github.com>
2024-09-18 18:26:41 +0000

    feat: add imports and exported function in main Module namespace

--- src/OntologyTrees.jl
+++ src/OntologyTrees.jl
@@ -1,5 +1,19 @@
 module OntologyTrees
 
-# Write your package code here.
+using Graphs
+using MetaGraphs
+using OntologyLookup
+using STRINGdb
+using TestItems
+
+include("tree.jl")
+export OntologyTree, graph, limit, required_terms, base
+
+include("utils.jl")
+export add_genes!, connect_term_genes!, connect_genes!, connect_term_gene!,
+       connect_term_genes!
+
+include("populate.jl")
+export add_genes!, connect_term_genes!
 
 end
