38724e3deeff64c457a416353ee61b119044831b
Christopher Damour <129872252+damourChris@users.noreply.github.com>
2024-09-18 17:37:43 +0000

    feat: add getters for onto trees

--- src/tree.jl
+++ src/tree.jl
@@ -5,3 +5,7 @@ struct OntologyTree
     max_parent_limit::Int
 end
 
+graph(onto_tree::OntologyTree) = onto_tree.graph
+limit(onto_tree::OntologyTree)::Int = onto_tree.max_parent_limit
+required_terms(onto_tree::OntologyTree)::Vector{Term} = onto_tree.required_terms
+base(onto_tree::OntologyTree)::Term = onto_tree.root
