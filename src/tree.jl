1c33a006e3c341a3e02e8c6ce165f7edd4d8181e
Christopher Damour <129872252+damourChris@users.noreply.github.com>
2024-09-18 17:38:32 +0000

    fix: update root -> base_term

--- src/tree.jl
+++ src/tree.jl
@@ -8,4 +8,4 @@ end
 graph(onto_tree::OntologyTree) = onto_tree.graph
 limit(onto_tree::OntologyTree)::Int = onto_tree.max_parent_limit
 required_terms(onto_tree::OntologyTree)::Vector{Term} = onto_tree.required_terms
-base(onto_tree::OntologyTree)::Term = onto_tree.root
+base(onto_tree::OntologyTree)::Term = onto_tree.base_term
